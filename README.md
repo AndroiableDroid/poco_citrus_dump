4cb347e93c8d9365864a17981f5f9cabafbf4970
AndroiableDroid <mohd.faraz.abc@gmail.com>
2020-12-16 15:32:45 +0530

    Add extras for qssi-user 10 QKQ1.200830.002 V12.0.1.0.QJFINXM release-keys
    Signed-off-by: AndroiableDroid <mohd.faraz.abc@gmail.com>

---
+++ README.md
@@ -0,0 +1,16 @@
+## qssi-user 10 QKQ1.200830.002 V12.0.1.0.QJFINXM release-keys
+- Manufacturer: xiaomi
+- Platform: bengal
+- Codename: citrus
+- Brand: POCO
+- Flavor: qssi-user
+- Release: 10
+- Id: QKQ1.200830.002
+- Incremental: V12.0.1.0.QJFINXM
+- Tags: release-keys
+- Fingerprint: POCO/citrus_in/citrus:10/QKQ1.200830.002/V12.0.1.0.QJFINXM:user/release-keys
+- Branch: qssi-user-10-QKQ1.200830.002-V12.0.1.0.QJFINXM-release-keys
+- Repo: poco_citrus_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cat bootimg/15_dtbdump_#&n1>.dtb.* 2>/dev/null >> bootimg/15_dtbdump_#&n1>.dtb
+rm -f bootimg/15_dtbdump_#&n1>.dtb.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/apex/com.android.runtime.release.apex.* 2>/dev/null >> system/system/apex/com.android.runtime.release.apex
+rm -f system/system/apex/com.android.runtime.release.apex.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/priv-app/Settings/Settings.apk.* 2>/dev/null >> product/priv-app/Settings/Settings.apk
+rm -f product/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat vendor/lib64/camera/components/com.qti.node.mialgocontrol.so.* 2>/dev/null >> vendor/lib64/camera/components/com.qti.node.mialgocontrol.so
+rm -f vendor/lib64/camera/components/com.qti.node.mialgocontrol.so.* 2>/dev/null
